be2e7bad4dfccc2d340064f1fd148237419bd89f
mishika-agarwal-1111 <mishikaagarwal1111@gmail.com>
2026-06-11 01:02:43 +0530

    string slicing

--- python-practice/day6.py
+++ python-practice/day6.py
@@ -1,13 +1,22 @@
-print("day 6 learning Python")
-a=31 
-print(type(a))
-b="31"
-print(type(b))
-c=float(a)
-print(type(c))
-a=int(input("enter a number"))
-b=int(input("enter another number"))
-print("the remainder of a and b is",a%b)
+# print("day 6 learning Python")
+# a=31 
+# print(type(a))
+# b="31"
+# print(type(b))
+# c=float(a)
+# print(type(c))
+# a=int(input("enter a number"))
+# b=int(input("enter another number"))
+# print("the remainder of a and b is",a%b)
+n="mishika"
+name=n[0:5]
+print(name) 
+name1=n[-1:-5:-1]
+print(name1)
+print(name.capitalize())
+
+
+
 
 
  
